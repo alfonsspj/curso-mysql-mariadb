037634e675019ef61c2eecadf12b3d4b723921d8
alfonsspj <alfonsspj@gmail.com>
2024-06-16 23:09:12 -0300

    Actualizando información

---
+++ 4-insert-lines.sql
@@ -0,0 +1,20 @@
+
+USE metro_cdmx;
+
+-- establece la codificación de caracteres de la conexión actual a utf8mb4
+-- SET NAMES utf8mb4;
+
+-- Insertar las líneas del metro
+INSERT INTO `lines` (name, color) VALUES
+("Línea 1", "Rosa"),
+("Línea 2", "Azul"),
+("Línea 3", "Verde Olivo"),
+("Línea 4", "Cian"),
+("Línea 5", "Amarillo"),
+("Línea 6", "Rojo"),
+("Línea 7", "Naranja"),
+("Línea 8", "Verde"),
+("Línea 9", "Café"),
+("Línea A", "Morado"),
+("Línea B", "Verde y Gris"),
+("Línea 12", "Oro");
